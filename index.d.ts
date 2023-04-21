f1d5d9c3c85b37689e9e93bc6a2be4930a3f4c4a
Srivishnu Ayyagari <srivishnua@lambdatest.com>
2023-04-21 16:22:57 +0530

    Updates the types for the new index.js

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,9 @@
-declare module 'webdriver-io-capture-it' {
+declare module 'wdio-capture-it' {
     import { Services } from '@wdio/types';
 
     class WdioCaptureIt implements Services.HookFunctions {
         constructor(config: object);
         beforeTest?(test: Services.HookFunctions.BeforeTestParam): void;
-        beforeCommand?(commandName: string, args: any[]): void;
-        afterCommand?(commandName: string, args: any[], result: any, error: Error): void;
     }
 
     export default WdioCaptureIt;
